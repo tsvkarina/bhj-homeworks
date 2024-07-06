627cf59c6c4e2aeec63c6a963f4e448b6c4958cf
tsvkarina <149406375+tsvkarina@users.noreply.github.com>
2024-07-06 21:23:42 +0300

    Update task - 3.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,13 +1,23 @@
-function startCountdown(seconds) {
-    let remainingTime = seconds;
-
-    const countdownInterval = setInterval(() => {
-        console.log(remainingTime); // Для отладки, чтобы видеть оставшееся время в консоли
-        if (remainingTime <= 0) {
-            clearInterval(countdownInterval);
-            alert("Вы победили в конкурсе!");
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const cookie = document.getElementById('cookie');
+    const counter = document.getElementById('clicker__counter');
+    
+    let clickCount = 0;
+    
+    const originalSize = 200;
+    const reducedSize = 180;
+    
+    cookie.onclick = function() {
+        clickCount++;
+        counter.textContent = clickCount;
+        
+        if (cookie.width === originalSize) {
+            cookie.width = reducedSize;
+            cookie.height = reducedSize;
         } else {
-            remainingTime -= 1;
+            cookie.width = originalSize;
+            cookie.height = originalSize;
         }
-    }, 1000);
-}
+    };
+});
